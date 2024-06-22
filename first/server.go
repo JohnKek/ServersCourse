f30472ffb572905820a87778108430c30d4c77e5
igta0222 <igor.tatarinov@netcracker.com>
2024-06-22 20:34:23 +0300

    Init

---
+++ first/server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+type MyHandler struct {
+}
+
+func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
+
+func main() {
+	err := http.ListenAndServe("localhost:3000", MyHandler{})
+	if err != nil {
+		panic(err)
+	}
+}
